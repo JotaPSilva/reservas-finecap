789fe241d175ec22baeaeda5e55694d3ce0d922b
João Pedro <jp101732@gmail.com>
2023-09-26 15:01:30 -0300

    Adicionar filtragem de reservas

--- finecap/views.py
+++ finecap/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import ReservaForm
 from .models import Reserva
+from datetime import datetime
 
 
-def lista_reservas(request):
+def lista_reservas(empresa):
+    nome_da_empresa = empresa.GET.get("nome_da_empresa", "")
+    quitado = empresa.GET.get("quitado", "")
+    stand_valor = empresa.GET.get("stand_valor", "")
+    data_reserva = empresa.GET.get("data_reserva", "")
+
     reservas = Reserva.objects.all()
+
+    if nome_da_empresa:
+        reservas = reservas.filter(nome__icontains=nome_da_empresa)
+
+    if quitado:
+        reservas = reservas.filter(quitado=quitado)
+
+    if stand_valor:
+        reservas = reservas.filter(stand__valor=stand_valor)
+
+    if data_reserva:
+        try:
+            # Converte uma string para um objeto datetime
+            date_obj = datetime.strptime(data_reserva, "%Y-%m-%d")
+            reservas = reservas.filter(data=date_obj)
+        except ValueError:
+            pass
+
     context = {"reservas": reservas}
-    return render(request, "lista_reservas.html", context)
+    return render(empresa, "lista_reservas.html", context)
 
 
 def criar_reserva(request):
